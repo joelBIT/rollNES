61031294b7226d7123ba86f73f68fb4af3016764
joelBit <joel.rollny@gmail.com>
2025-11-10 08:37:05 +0100

    Installed react-router and created LandingPage

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import { URL_HOME } from "./utils";
+import LandingPage from "./pages/LandingPage";
+import { App } from "../src/App";
+
+export const routes = createBrowserRouter([
+    {
+        path: URL_HOME,
+        Component: App,
+        children: [
+            {
+                index: true,
+                Component: LandingPage
+            }
+        ],
+    },
+]);
