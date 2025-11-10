d20f4903988bf3f29cf39fb810789e25c0c0ec6f
joelBit <joel.rollny@gmail.com>
2025-11-10 08:46:20 +0100

    Created Carousel component

---
+++ src/components/header/Carousel.tsx
@@ -0,0 +1,44 @@
+import {useEffect, useState, type ReactElement} from "react";
+import { NavLink } from "react-router";
+
+import "./Carousel.css";
+
+export function Carousel(): ReactElement {
+    const [ slide, setSlide ] = useState<number>(0);
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            nextSlide();
+        }, 7000);
+
+        return () => {
+            clearInterval(id);
+        };
+    }, [slide]);
+
+    function nextSlide(): void {
+        setSlide(slide === 2 ? 0 : slide + 1);
+    }
+
+    function prevSlide(): void {
+        setSlide(slide === 0 ? 2 : slide - 1);
+    }
+
+    return (
+        <section id="carousel">
+            <button id="prevButton" onClick={prevSlide}>
+                <span className="material-symbols-outlined arrow"> arrow_back_ios </span>
+            </button>
+
+            <section id="slide-space">
+                <h1 className={slide === 0 ? "active" : "inactive"}> FREE DOMESTIC SHIPPING OVER &#8364;50 & EASY RETURNS </h1>
+                <h1 className={slide === 1 ? "active" : "inactive"}> NEW: BATTLE TOADS VS DOUBLE DRAGON <NavLink to="/games/9"> SHOP NOW </NavLink> </h1>
+                <h1 className={slide === 2 ? "active" : "inactive"}> REST: Buy one, get one 50% off* | CODE: REST50 </h1>
+            </section>
+
+            <button id="nextButton" onClick={nextSlide}>
+                <span className="material-symbols-outlined arrow"> arrow_forward_ios </span>
+            </button>
+        </section>
+    );
+}
