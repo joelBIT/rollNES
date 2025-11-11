f07c2d6d85cb03637bc59c9f2f96222ff888ea47
joelBit <joel.rollny@gmail.com>
2025-11-11 13:24:07 +0100

    Remove game from favourites in FavouritesMenu

--- src/components/favourites/FavouriteCard.tsx
+++ src/components/favourites/FavouriteCard.tsx
@@ -2,6 +2,7 @@ import { type ReactElement } from "react";
 import { NavLink } from "react-router";
 import type { Game } from "../../types/types";
 import { COVER_URL, URL_GAMES_PAGE } from "../../utils";
+import { useFavourites } from "../../hooks/useFavourites";
 
 import "./FavouriteCard.css";
 
@@ -9,10 +10,7 @@ import "./FavouriteCard.css";
  * A favourite game. A FavouriteCard represents a game in the FavouriteMenu.
  */
 export function FavouriteCard({game, close}: {game: Game, close: () => void}): ReactElement {
-
-    function removeGame(id: number): void {
-        console.log(id);
-    }
+    const { removeFavouriteById } = useFavourites();
 
     return (
         <section className="favourite-card">
@@ -30,8 +28,9 @@ export function FavouriteCard({game, close}: {game: Game, close: () => void}): R
                 > 
                     {game.title} 
                 </NavLink>
-    
-                <h2 className="favourite-card__button" onClick={() => removeGame(game.id)}> Remove </h2>
+
+                <h3 className="favourite-card__players"> {game.players} player{game.players > 1 ? "s" : ""} </h3>
+                <h2 className="favourite-card__button" onClick={() => removeFavouriteById(game.id)}> Remove </h2>
             </section>
         </section>
     );
