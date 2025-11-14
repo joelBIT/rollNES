9cf4ff3036906a3c3b7aeb7b7ebda68334e07f6b
joelBit <joel.rollny@gmail.com>
2025-11-14 08:33:54 +0100

    Update rendered game cards when releasing the slider knob

--- src/components/game/RangeSlider.tsx
+++ src/components/game/RangeSlider.tsx
@@ -1,24 +1,28 @@
-import { useState, type ChangeEvent, type ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 
 import "./RangeSlider.css";
 
 /**
- * Slider for changing how many games are rendered on the GamesPage.
+ * Slider for changing how many games are rendered on the GamesPage. 
+ * When mouse state is "up" the parent is called with that chosen value (avoids parent rerendering all the time when sliding).
  */
 export function RangeSlider({min, max, setSliderValue}: {min: number, max: number, setSliderValue: (value: number) => void}): ReactElement {
     const [value, setValue] = useState<number>(min);
 
-    function handleInput(event : ChangeEvent<HTMLInputElement>): void {
-        const value = parseInt(event.target.value);
-        setValue(value);
-        setSliderValue(value);
-    }
-
     return (
         <section className="rangeSlider">
-            <input type="range" min={min} step="1" max={max} value={value} className="slider" onInput={handleInput} />
+            <input 
+                type="range" 
+                min={min} 
+                step="1" 
+                max={max} 
+                value={value >= max ? max : value} 
+                className="slider" 
+                onInput={(e) => setValue(parseInt((e.target as HTMLInputElement).value))} 
+                onMouseUp={(e) => setSliderValue(parseInt((e.target as HTMLInputElement).value))} 
+            />
 
-            <h2 className="slider-text"> Show at most {value} / {max} games</h2>
+            <h2 className="slider-text"> Showing {value >= max ? max : value} / {max} games</h2>
         </section>
     )
 }
