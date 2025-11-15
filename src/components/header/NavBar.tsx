e1eeb2e50769419b0a278a41c5fcf685dc9015cc
joelBit <joel.rollny@gmail.com>
2025-11-15 12:34:59 +0100

    Changed font in Navbar for games and config

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -15,13 +15,13 @@ export function NavBar(): ReactElement {
                     <li className="navbar__list-element">
                         <NavLink to={URL_GAMES_PAGE}>
                             <span className="material-symbols-outlined" title="Game list"> list_alt </span>
-                            <h3>Games</h3>
+                            <h3 className="navbar__list-title">Games</h3>
                         </NavLink>
                     </li>
 
                     <li className="navbar__list-element" onClick={() => setOpenModal(true)}>
                         <span className="material-symbols-outlined" title="Game controllers"> videogame_asset </span>
-                        <h3>Config</h3>
+                        <h3 className="navbar__list-title">Config</h3>
                     </li>
                 </ul>
             </nav>
