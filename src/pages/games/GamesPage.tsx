68ef42435f1c100bed45e812154a3b2629208158
joelBit <joel.rollny@gmail.com>
2025-11-15 06:51:18 +0100

    Replaced the other filters with FilterAccordion

--- src/pages/games/GamesPage.tsx
+++ src/pages/games/GamesPage.tsx
@@ -8,10 +8,8 @@ import "./GamesPage.css";
  * Page for filtering playable games. A user can choose how many of the filtered games to show on the page.
  */
 export default function GamesPage(): ReactElement {
-    const { filteredGames, allCategories, allPlayers, allPublishers, allDevelopers, matchesFilter, addFilter, removeFilter } = useGames();
+    const { filteredGames, allCategories, allPlayers, allPublishers, allDevelopers } = useGames();
     const [isShowingRange, setIsShowingRange] = useState(false);
-    const [isShowingPublishers, setIsShowingPublishers] = useState(false);
-    const [isShowingDevelopers, setIsShowingDevelopers] = useState(false);
     const [minRange] = useState<number>(filteredGames?.length > 10 ? 10 : filteredGames.length);     // Minimum 10 games are rendered on the page (or fewer due to chosen filters)
     const [numberGamesShowing, setNumberGamesShowing] = useState<number>(minRange);
 
@@ -35,61 +33,9 @@ export default function GamesPage(): ReactElement {
 
                 <FilterAccordion values={allPlayers()} title={"Players"} filterName={"players"} />
 
-                <section className="game-filters-panel__accordion">
-                    <article className="filter-card-header" onClick={() => setIsShowingPublishers(!isShowingPublishers)}> 
-                        <span className={isShowingPublishers ? "rotate-down" : "rotate-up"}> &#94; </span>
-                        <h5 className="filter-card-title"> Publisher </h5>
-                    </article>
+                <FilterAccordion values={allPublishers()} title={"Publisher"} filterName={"publisher"} />
 
-                    {
-                        allPublishers().map(publisher => 
-                            <article className={isShowingPublishers ? "filter-card-collapsible dropdown" : "filter-card-collapsible"} key={publisher}>
-                                <section className="filter-card-body">
-                                    <section className="filter-card-body-data">
-                                        <input 
-                                            type="checkbox" 
-                                            name={`category-${publisher}`} 
-                                            id={`category-${publisher}`} 
-                                            onClick={(e) => (e.target as HTMLInputElement).checked ? addFilter("publisher", publisher) : removeFilter("publisher", publisher)} 
-                                        />
-
-                                        <h3 className="filter-card-body-data__title"> {publisher} </h3>
-                                    </section>
-                                    
-                                    <h3 className="filter-card-body-data__amount"> { matchesFilter("publisher", publisher) } </h3>
-                                </section>
-                            </article>
-                        )
-                    }
-                </section>
-
-                <section className="game-filters-panel__accordion">
-                    <article className="filter-card-header" onClick={() => setIsShowingDevelopers(!isShowingDevelopers)}> 
-                        <span className={isShowingDevelopers ? "rotate-down" : "rotate-up"}> &#94; </span>
-                        <h5 className="filter-card-title"> Developer </h5>
-                    </article>
-
-                    {
-                        allDevelopers().map(developer => 
-                            <article className={isShowingDevelopers ? "filter-card-collapsible dropdown" : "filter-card-collapsible"} key={developer}>
-                                <section className="filter-card-body">
-                                    <section className="filter-card-body-data">
-                                        <input 
-                                            type="checkbox" 
-                                            name={`category-${developer}`} 
-                                            id={`category-${developer}`} 
-                                            onClick={(e) => (e.target as HTMLInputElement).checked ? addFilter("developer", developer) : removeFilter("developer", developer)} 
-                                        />
-
-                                        <h3 className="filter-card-body-data__title"> {developer} </h3>
-                                    </section>
-                                    
-                                    <h3 className="filter-card-body-data__amount"> { matchesFilter("developer", developer) } </h3>
-                                </section>
-                            </article>
-                        )
-                    }
-                </section>
+                <FilterAccordion values={allDevelopers()} title={"Developer"} filterName={"developer"} />
             </section>
 
             <section id="games-list">
