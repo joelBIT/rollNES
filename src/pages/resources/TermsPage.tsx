938df67f390195d42105dd0280d0eb4a9393125d
joelBit <joel.rollny@gmail.com>
2025-11-16 13:09:17 +0100

    Updated text on TermsPage

--- src/pages/resources/TermsPage.tsx
+++ src/pages/resources/TermsPage.tsx
@@ -19,14 +19,14 @@ export default function TermsPage(): ReactElement {
                 <p className="terms-sections__text">
                     By playing a game at the RollNES site, you warrant that you own that game and accept these terms & conditions which shall apply to 
                     all interactions with the RollNES emulator and this site. None of these terms & conditions affect your statutory rights. 
-                    No other terms or changes to the terms & conditions shall be binding unless agreed in writing signed by us.
+                    No other terms or changes to the terms & conditions shall be binding unless agreed in writing signed by RollNES.
                 </p>
 
                  <ul className="terms-sections__list">
                     <li>By using the RollNES emulator, you agree to be bound by these terms & conditions.</li>
                     <li>By playing a game at this site, you warrant that you are an owner of that specific game.</li>
-                    <li>All personal information you provide us with or that we obtain will be handled by us as responsible for the personal information.</li>
-                    <li>Events outside our control shall be considered force majeure.</li>
+                    <li>All personal information you provide RollNES with or that RollNES obtain will be handled by RollNES as responsible for the personal information.</li>
+                    <li>Events outside RollNES control shall be considered force majeure.</li>
                     <li>RollNES reserves the right to amend any information without prior notice.</li>
                 </ul>
             </section>
