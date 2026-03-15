d0b813031e06a92e4638def4482c1690e6dc7ac1
joelBit <joel.rollny@gmail.com>
2026-03-15 16:54:06 +0100

    Map over all games from parameter

--- src/components/home/PopularGames.tsx
+++ src/components/home/PopularGames.tsx
@@ -9,7 +9,7 @@ import "./PopularGames.css";
  * One of the most popular games shown in the Landing page.
  */
 export function PopularGames({games}: {games: Game[]}): ReactElement {
-    const [selectedCover, setSelectedCover] = useState<string>(games[0].cover);
+    const [selectedCover, setSelectedCover] = useState<string>(games[0].cover ?? "");
 
     return (
         <section className="popular-games">
@@ -17,37 +17,17 @@ export function PopularGames({games}: {games: Game[]}): ReactElement {
                 <section className="popular-games__links">
                     <h2 className="popular-games__heading"> Most Played </h2>
 
-                    <NavLink 
-                        to={URL_GAMES_PAGE + "/" + games[0].id} 
-                        className={games[0].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
-                        onMouseOver={() => setSelectedCover(games[0].cover)}
-                    > 
-                        {games[0].title} 
-                    </NavLink>
-
-                    <NavLink 
-                        to={URL_GAMES_PAGE + "/" + games[1].id} 
-                        className={games[1].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
-                        onMouseOver={() => setSelectedCover(games[1].cover)}
-                    > 
-                        {games[1].title} 
-                    </NavLink>
-
-                    <NavLink 
-                        to={URL_GAMES_PAGE + "/" + games[2].id} 
-                        className={games[2].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
-                        onMouseOver={() => setSelectedCover(games[2].cover)}
-                    > 
-                        {games[2].title} 
-                    </NavLink>
-
-                    <NavLink 
-                        to={URL_GAMES_PAGE + "/" + games[3].id} 
-                        className={games[3].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
-                        onMouseOver={() => setSelectedCover(games[3].cover)}
-                    > 
-                        {games[3].title} 
-                    </NavLink> 
+                    {
+                        games.map((game: Game) => 
+                            <NavLink 
+                                to={URL_GAMES_PAGE + "/" + game.id} 
+                                className={game.cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
+                                onMouseOver={() => setSelectedCover(game.cover)}
+                            > 
+                                {game.title} 
+                            </NavLink>
+                        )
+                    }
                 </section>
 
                 <figure className="popular-games__figure">
