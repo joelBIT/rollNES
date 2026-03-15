26fcc05e9dfcbdc20a1e45a5ab9f51b63aa1787c
joelBit <joel.rollny@gmail.com>
2026-03-15 15:34:17 +0100

    Added links to component and change image when hovering links

--- src/components/home/PopularGames.tsx
+++ src/components/home/PopularGames.tsx
@@ -1,4 +1,4 @@
-import type { ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 import { NavLink } from "react-router";
 import type { Game } from "../../types/types";
 import { COVER_URL, URL_GAMES_PAGE } from "../../utils";
@@ -9,15 +9,49 @@ import "./PopularGames.css";
  * One of the most popular games shown in the Landing page.
  */
 export function PopularGames({games}: {games: Game[]}): ReactElement {
+    const [selectedCover, setSelectedCover] = useState<string>(games[0].cover);
+
     return (
-        <section className="popular-game-card">
-            <section>
-                {games.length}
+        <section className="popular-games">
+            <section className="popular-games__links">
+                <h2 className="popular-games__heading"> Most Played </h2>
+
+                <NavLink 
+                    to={URL_GAMES_PAGE + "/" + games[0].id} 
+                    className="popular-game-link" 
+                    onMouseOver={() => setSelectedCover(games[0].cover)}
+                > 
+                    {games[0].title} 
+                </NavLink>
+
+                <NavLink 
+                    to={URL_GAMES_PAGE + "/" + games[1].id} 
+                    className="popular-game-link" 
+                    onMouseOver={() => setSelectedCover(games[1].cover)}
+                > 
+                    {games[1].title} 
+                </NavLink>
+
+                <NavLink 
+                    to={URL_GAMES_PAGE + "/" + games[2].id} 
+                    className="popular-game-link" 
+                    onMouseOver={() => setSelectedCover(games[2].cover)}
+                > 
+                    {games[2].title} 
+                </NavLink>
+
+                <NavLink 
+                    to={URL_GAMES_PAGE + "/" + games[3].id} 
+                    className="popular-game-link" 
+                    onMouseOver={() => setSelectedCover(games[3].cover)}
+                > 
+                    {games[3].title} 
+                </NavLink> 
             </section>
 
-            <NavLink to={URL_GAMES_PAGE + "/" + games[0].id} className="popular-game-image"> GO </NavLink> 
-
-            <img src={COVER_URL + games[0].cover} alt="Game cover" />
+            <figure className="popular-games__figure">
+                <img src={COVER_URL + selectedCover} alt="Game cover" />
+            </figure>
         </section>
     );
 }
