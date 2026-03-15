79837ab435cda1ec1d4e7da5161ffa3f0469f80d
joelBit <joel.rollny@gmail.com>
2026-03-15 16:38:31 +0100

    Change color of selected game

--- src/components/home/PopularGames.tsx
+++ src/components/home/PopularGames.tsx
@@ -13,45 +13,47 @@ export function PopularGames({games}: {games: Game[]}): ReactElement {
 
     return (
         <section className="popular-games">
-            <section className="popular-games__links">
-                <h2 className="popular-games__heading"> Most Played </h2>
+            <section className="popular-games__content">
+                <section className="popular-games__links">
+                    <h2 className="popular-games__heading"> Most Played </h2>
 
-                <NavLink 
-                    to={URL_GAMES_PAGE + "/" + games[0].id} 
-                    className="popular-game-link" 
-                    onMouseOver={() => setSelectedCover(games[0].cover)}
-                > 
-                    {games[0].title} 
-                </NavLink>
+                    <NavLink 
+                        to={URL_GAMES_PAGE + "/" + games[0].id} 
+                        className={games[0].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
+                        onMouseOver={() => setSelectedCover(games[0].cover)}
+                    > 
+                        {games[0].title} 
+                    </NavLink>
 
-                <NavLink 
-                    to={URL_GAMES_PAGE + "/" + games[1].id} 
-                    className="popular-game-link" 
-                    onMouseOver={() => setSelectedCover(games[1].cover)}
-                > 
-                    {games[1].title} 
-                </NavLink>
+                    <NavLink 
+                        to={URL_GAMES_PAGE + "/" + games[1].id} 
+                        className={games[1].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
+                        onMouseOver={() => setSelectedCover(games[1].cover)}
+                    > 
+                        {games[1].title} 
+                    </NavLink>
 
-                <NavLink 
-                    to={URL_GAMES_PAGE + "/" + games[2].id} 
-                    className="popular-game-link" 
-                    onMouseOver={() => setSelectedCover(games[2].cover)}
-                > 
-                    {games[2].title} 
-                </NavLink>
+                    <NavLink 
+                        to={URL_GAMES_PAGE + "/" + games[2].id} 
+                        className={games[2].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
+                        onMouseOver={() => setSelectedCover(games[2].cover)}
+                    > 
+                        {games[2].title} 
+                    </NavLink>
 
-                <NavLink 
-                    to={URL_GAMES_PAGE + "/" + games[3].id} 
-                    className="popular-game-link" 
-                    onMouseOver={() => setSelectedCover(games[3].cover)}
-                > 
-                    {games[3].title} 
-                </NavLink> 
+                    <NavLink 
+                        to={URL_GAMES_PAGE + "/" + games[3].id} 
+                        className={games[3].cover === selectedCover ? "popular-game-link selected-game" : "popular-game-link"} 
+                        onMouseOver={() => setSelectedCover(games[3].cover)}
+                    > 
+                        {games[3].title} 
+                    </NavLink> 
+                </section>
+
+                <figure className="popular-games__figure">
+                    <img src={COVER_URL + selectedCover} alt="Game cover" />
+                </figure>
             </section>
-
-            <figure className="popular-games__figure">
-                <img src={COVER_URL + selectedCover} alt="Game cover" />
-            </figure>
         </section>
     );
 }
