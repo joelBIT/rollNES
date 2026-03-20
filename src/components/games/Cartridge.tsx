9feed65ab114ef7ff6c89fc6f4c4bf10cc4a2dab
joelBit <joel.rollny@gmail.com>
2026-03-20 14:18:35 +0100

    Created Cartridge component

---
+++ src/components/games/Cartridge.tsx
@@ -0,0 +1,51 @@
+import type { ReactElement } from "react";
+
+import "./Cartridge.css";
+
+/**
+ * A cartridge on the GamePage showing the NES game cover.
+ */
+export function Cartridge({coverUrl}: {coverUrl: string}): ReactElement {
+    return (
+        <section id="cartridge">
+            <div id="wrapper">
+                <div className="cartridge">
+                    <div>
+                        <section className="front">
+                            <img src={coverUrl} id="cover-pic" alt="nes cover" />
+                            <span className="triangle-down" />
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li id="lastLine"></li>
+                            </ul>
+                        </section>
+                    </div>
+
+                    <div className="top"></div>
+                    <div className="bottom"></div>
+                </div>
+            </div>
+        </section>
+    );
+}
