5c358b4d92f582a37936d9868c95aced5348fffe
joelBit <joel.rollny@gmail.com>
2026-03-29 16:29:52 +0200

    added background to GameCard

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -16,28 +16,37 @@ export function GameCard({game}: { game: Game}): ReactElement {
 
     return (
         <section className="game-card">
-            <section className="game-image">
-                <Cartridge coverUrl={COVER_URL + game.cover} />
-            </section>
+            <img src={COVER_URL + game.cover} className="game-card__background" />
 
-            <section className="game-information">
-                <NavLink to={URL_GAMES_PAGE + "/" + game.id} className="game-information__link" title="View game"> 
-                    <h2 className="game-information__heading"> {game.title} </h2> 
-                </NavLink>
-                
-                <h2 className="game-information__description"> 
-                    {game.description.substring(0, 300)}{game.description.length > 250 ? "..." : ""} 
-                </h2>
-            </section>
-            
-            <section className="game-details">
-                <section className="game-details__reviews">
-                    <Rating rating={getAverageRating(game.reviews)} />
-                    <p> {game.reviews?.length} review{game.reviews?.length > 1 || game.reviews?.length === 0 ? "s" : ""} </p>
+            <section className="game-card__content">
+                <section className="game-image">
+                    <Cartridge coverUrl={COVER_URL + game.cover} />
                 </section>
 
-                <section className="game-favourite-icon" onClick={favourite ? () => removeFavouriteById(game.id) : () => addFavourite(game)}>
-                    {favourite ? <h2>&#x2764;&#xfe0f;</h2> : <span className="material-symbols-outlined"> favorite </span>}
+                <section className="game-information">
+                    <NavLink to={URL_GAMES_PAGE + "/" + game.id} className="game-information__link" title="View game"> 
+                        <h2 className="game-information__heading"> {game.title} </h2> 
+                    </NavLink>
+                    
+                    <h2 className="game-information__description"> 
+                        {game.description.substring(0, 300)}{game.description.length > 250 ? "..." : ""} 
+                    </h2>
+                </section>
+                
+                <section className="game-details">
+                    <section className="game-details__reviews">
+                        <Rating rating={getAverageRating(game.reviews)} />
+                        <p> {game.reviews?.length} review{game.reviews?.length > 1 || game.reviews?.length === 0 ? "s" : ""} </p>
+                    </section>
+
+                    <section className="game-favourite-icon" onClick={favourite ? () => removeFavouriteById(game.id) : () => addFavourite(game)}>
+                        {
+                            favourite ? 
+                                <h2 className="favourite-heart">&#x2764;&#xfe0f;</h2> 
+                                : 
+                                <span className="material-symbols-outlined favourite-heart"> favorite </span>
+                        }
+                    </section>
                 </section>
             </section>
         </section>
