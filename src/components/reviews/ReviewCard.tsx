654d6963c67d6248a7c1ceae44915d5827993077
joelBit <joel.rollny@gmail.com>
2026-08-12 10:45:04 +0200

    Added placeholder profile image to review card

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -5,11 +5,17 @@ import { convertDateToString } from "../../utils";
 
 import "./ReviewCard.css";
 
+/**
+ * TODO: Add profile image to Review so that the img element uses the profile image if user is logged in when creating the review, or
+ * use the default profile image if the review is created by someone who is not signed in.
+ */
 export function ReviewCard({review}: {review: Review}): ReactElement {
 
     return (
         <section className="review-card">
             <section className="review-top">
+                <img src="/github.svg" alt="Profile image" className="profile-image" />
+
                 <section className="reviewer">
                     <h2 className="review-name"> {review.reviewer_name} </h2>
                     <h2 className="review-date"> Reviewed {convertDateToString(new Date(review.date))} </h2>
