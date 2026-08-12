87ce862cc3f745f7ceab64a6d6dda09bb39996dd
joelBit <joel.rollny@gmail.com>
2026-08-12 10:17:28 +0200

    Changed styling of review card

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -1,28 +1,27 @@
 import type { ReactElement } from "react";
 import type { Review } from "../../types/types";
 import { Rating } from "..";
+import { convertDateToString } from "../../utils";
 
 import "./ReviewCard.css";
 
 export function ReviewCard({review}: {review: Review}): ReactElement {
 
-    function convertDate(date: string): string {
-        return new Date(date).toDateString();
-    }
-
     return (
         <section className="review-card">
-            <section className="reviewer">
-                <h2 className="review-name"> {review.reviewer_name} </h2>
-                <h2 className="review-date"> {convertDate(review.date)} </h2>
-                { review.reviewer_id ? <h3 className="review-verified"> Verified User </h3> : <></>}
+            <section className="review-top">
+                <section className="reviewer">
+                    <h2 className="review-name"> {review.reviewer_name} </h2>
+                    <h2 className="review-date"> Reviewed {convertDateToString(new Date(review.date))} </h2>
+                    { review.reviewer_id ? <h3 className="review-verified"> Verified User </h3> : <></>}
+                </section>
+
+                <Rating rating={review.rating} />
             </section>
             
             <section className="review-content">
-                <Rating rating={review.rating} />
-
-                <h2 className="review-heading"> {review.heading} </h2>
-                <h2 className="review-text"> {review.review} </h2>
+                <h2 className="review-heading">{review.heading} </h2>
+                <h2 className="review-text">{review.review} </h2>
             </section>
         </section>
     );
