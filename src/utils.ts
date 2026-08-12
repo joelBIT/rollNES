cb2ff18ba26302f8e19d9a5fa194e66a5a4ef2e9
joelBit <joel.rollny@gmail.com>
2026-08-12 09:29:06 +0200

    Changed styling of game details

--- src/utils.ts
+++ src/utils.ts
@@ -66,3 +66,41 @@ export function isMobileDevice(): boolean {
 
     return false;
 }
+
+export function convertDateToString(date: Date) {
+    return `${getMonthText(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
+}
+
+/**
+ * Converts the supplied month (number) into the corresponding month as a text representation.
+ */
+export function getMonthText(month: number): string {
+    switch(month) {
+        case 0:
+            return "January";
+        case 1:
+            return "February";
+        case 2:
+            return "March";
+        case 3:
+            return "April";
+        case 4:
+            return "May";
+        case 5:
+            return "June";
+        case 6:
+            return "July";
+        case 7:
+            return "August";
+        case 8:
+            return "September";
+        case 9:
+            return "October";
+        case 10:
+            return "November";
+        case 11:
+            return "December";
+        default:
+            return "";
+    }
+}
